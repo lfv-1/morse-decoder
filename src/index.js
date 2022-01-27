32a054aca84a956ac3eca37c7121023868eff7bf
lfv-1 <nega123497@gmail.com>
2022-01-27 22:19:54 +0300

    feat:completed

--- src/index.js
+++ src/index.js
@@ -38,7 +38,28 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = '';
+    for (let i = 0; i < expr.length; i += 10) {
+        let string = expr.substr(i, 10);
+        if (string == '**********') {
+            result += ' ';
+        } else {       
+            let stringMorse = ''
+            for (let j = 0; j < string.length; j += 2) {
+                let x = string.substr(j, 2);
+                if (x == '10') {
+                    xMorse = '.';
+                } else if (x == '11') {
+                    xMorse = '-';
+                } else {
+                    continue;
+                }
+                stringMorse += xMorse;
+            }
+            result += MORSE_TABLE[stringMorse];
+        }  
+    }
+    return result;
 }
 
 module.exports = {
